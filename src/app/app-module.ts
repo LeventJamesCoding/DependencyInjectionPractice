de946eac4455d5c0516d24df7bdd7f8b864d1f7b
LeventJamesCoding <leventenespiskin@gmail.com>
2025-09-24 14:47:13 +0300

    24 September 2025

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -4,6 +4,8 @@ import { ProductService } from './productservice';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { productServiceIT } from './injection-token';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -11,11 +13,25 @@ import { App } from './app';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
-    ProductService
+    //-------Dependency Injection Tokens-------
+    //ProductService, //Defult Type Token
+    //{provide:ProductService, useClass:ProductService}, //Type Token
+    //{provide:"productService", useClass:ProductService} //String Token
+    //{provide: productServiceIT, useClass:ProductService} 
+    //{provide: "example", useValue: () => {return "merhaba"}},
+    {
+      provide: "productService", useFactory: (httpClient: HttpClient) => {
+        const obs = httpClient.get("https://jsonplaceholder.typicode.com/posts");
+        const data = await firstValueFrom(obs);
+        console.log(data);
+        return ProductService;
+      }, deps:[HttpClient]
+    }
   ],
   bootstrap: [App]
 })
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,15 @@
 import { Component, signal, Inject } from '@angular/core';
 import { ProductService } from './productservice';
+import { productServiceIT } from './injection-token';
 
 @Component({
   selector: 'app-root',
   template: ``,
   standalone: false,
   styleUrl: './app.scss',
-})
-export class App {
-  constructor(@Inject(ProductService) private productService:ProductService){
-    console.log(productService.getProducts());
-  }
+}) 
+export class App {    
+  //constructor(@Inject(productServiceIT) private productService:ProductService, @Inject("example") func:any){
+  //  console.log(productService.getProducts());
+  //  console.log(func());
 }
